9475b2f04c02fffbf89631d9dc18db767af23096
rahulptl556 <rahulptl556@gmail.com>
2023-03-23 20:13:06 +0530

    main

--- index.js
+++ index.js
@@ -15,6 +15,7 @@
 // // removeEventListner
 // document.removeEventListener('click',eventListner);
 
+/*
 // Document Fragment
 const t1 = performance.now();
 
@@ -44,4 +45,130 @@ let myDiv = document.createElement('div');
 
  const t4 = performance.now();
 console.log('Time taken -> ' + (t4-t3) + 'ms');
+*/
+
+// setTimeout Code 
+/*
+   function name (){
+      console.log('Hi');
+   }
+
+   setTimeout( function() {
+      console.log('hello');
+   }, 4000);
+*/
+
+         // ASYNC 
+/*
+   setTimeout(function(){
+      console.log("third");
+   },3000)
+
+   function first(){
+      console.log("First");
+   }
+   first();
+
+   console.log("second");
+*/
+
+                  // Promises 
+/*
+   let firstPromise = new Promise(function(resolve,reject){
+      setTimeout(function(){
+         console.log("Hi Im inside the promise");
+      },5000);
+
+      resolve();
+   // reject(new Error("sirji error aa rahe h"));
+   })
+*/
+
+           // then() & catch() method
+/*
+   let p = new Promise(function(resolve,reject){
+      setTimeout(function(){
+         console.log("This is me");
+      },3000);
+      //resolve(912313);
+      reject(new Error("sirji error aa rahe h"));
+   })
+
+   p.then((value) => {console.log(value)}, (error) => {console.log("Recieved Errors")});
+
+   //p.catch((error) => {console.log("Recieved an error")});
+
+   console.log("Hello");
+*/
+
+        // Playing with 2 promises
+/*
+   let p1 = new Promise(function(resolve,reject){
+      setTimeout(() => {console.log("Promise1 Started")},2000 );
+      resolve();
+   })
+
+   let output = p1.then(() => {
+      let p2 = new Promise(function(resolve,reject){
+         setTimeout(() => {console.log("Promise2 Started")},3000 );
+         resolve("wada 2 resolved")
+      });
+      return p2;
+   });
+
+   output.then((value => {console.log(value)}));
+*/
+
+      //ASYNC Function
+// async function abcd(){
+//    return 7;
+// }
+// console.log(abcd)
+
+//ASYNC AWAIT
+
+// async function weather(){
+   
+//    let delhi = new Promise((resolve,reject) => {
+//       setTimeout(() => {
+//          resolve("Delhi is Hot")
+//       },2000);
+//    })
+   
+      
+//    let hyd = new Promise((resolve,reject) => {
+//       setTimeout(() => {
+//          resolve("Hyd is cold")
+//       },4000);
+//    })
+
+//    let dM =  delhi;
+//    let hM = await hyd;
+
+//    return [dM,hM];
+
+// }
+
+// CLOSURE
+
+// function outer(){
+//    console.log("Hi");
+//    let a = 5;
+   
+//    function inner(){
+//       let a = 3;
+//       console.log(a);
+//       inner1()
+//          function inner1(){
+//             let a = 7;
+//             console.log(a);
+//             inner2()
+//                function inner2(){
+//                   console.log(a);
+//                }
+//          }
+//    }
+//    return inner();
+// }
+// outer();
 
